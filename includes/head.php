60d0bac141f89c1e5958a1cfd0e0de2a83c5a251
zalamanga <rafikmas78@gmail.com>
2026-04-23 14:54:55 +0700

    Hotfix: restore Tailwind CDN script accidentally removed in SEO commit

--- includes/head.php
+++ includes/head.php
@@ -79,7 +79,9 @@ $__page_image       = isset($og_image) ? $og_image : $__base_url . '/img/logo.pn
     </script>
     
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
-    
+
+    <script src="https://cdn.tailwindcss.com"></script>
+
     <script>
         tailwind.config = {
             theme: {
